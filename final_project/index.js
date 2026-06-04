c5c9a49c4ba355758773776089dadef2a92caa6a
Riyan Ahmad Khan <riyan085@proton.me>
2026-06-04 13:20:07 +0530

    Refactor authentication and session handling

--- final_project/index.js
+++ final_project/index.js
@@ -1,22 +1,58 @@
+// Importing Packages
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const session = require('express-session')
-const customer_routes = require('./router/auth_users.js').authenticated;
-const genl_routes = require('./router/general.js').general;
 
+// Pre-Config
 const app = express();
-
+app.use((req, res, next)=>{
+    res.setHeader("Access-Control-Allow-Origin",'http://localhost:3000');
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  })
 app.use(express.json());
-
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
-
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
-});
- 
 const PORT =5000;
 
-app.use("/customer", customer_routes);
-app.use("/", genl_routes);
+// Routes
+const general_routes = require('./routes/general.js');
+const customer_routes = require('./routes/auth_users.js');
+// const customer_routes = require('./routes/customer.js');
+// const vendor_routes = require('./routes/vendor.js');
+
+
+
+//Middleware for general user (without authetication)
+app.use("/general", general_routes);
+
+
+app.use(
+    "/customer",
+    session({
+      secret: "fingerprint_customer",
+      resave: true,
+      saveUninitialized: true,
+    })
+  );
+
+app.use("/customer/auth/*", function auth(req, res, next) {
+    if (req.session.authorization) {
+      let token = req.session.authorization["accessToken"];
+  
+      jwt.verify(token, "access", (err, user) => {
+        if (!err) {
+          req.user = user;
+          next();
+        } else {
+          return res.status(403).json({ message: "User not authenticated." });
+        }
+      });
+    } else {
+      return res.status(403).json({ message: "User not logged in." });
+    }
+  });
+
 
 app.listen(PORT,()=>console.log("Server is running"));
